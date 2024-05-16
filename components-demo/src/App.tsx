5a6e3ed9220a72d350543aeb6ded5c41c3f28758
ILzat <ilzat.sadykov.2006@bk.ru>
2024-05-16 17:13:06 +0600

    Init app

---
+++ components-demo/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Person from "./components/Person/Person";
+import {useState} from "react";
+
+
+function App() {
+
+  return (
+      <>
+
+      </>
+  )
+}
+
+export default App
