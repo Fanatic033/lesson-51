c374d5c0855d4fed4460e0bf249eaaf1d9e5573e
ILzat <ilzat.sadykov.2006@bk.ru>
2024-05-16 17:16:11 +0600

    app update

--- components-demo/src/App.tsx
+++ components-demo/src/App.tsx
@@ -3,12 +3,27 @@ import './App.css'
 import Person from "./components/Person/Person";
 import {useState} from "react";
 
-
+interface IPerson{
+    name: string;
+    age: number;
+}
 function App() {
+    const [people,setPeople] = useState<IPerson[]>([
+        {name: "John", age: 30},
+        {name: "Jan", age: 20},
+    ]);
+const changeName = () => {
+    const copyPeople = [...people];
+    copyPeople[0].name = "ilzat";
+    copyPeople[1].name = "Jan";
+    setPeople(copyPeople);
 
+}
   return (
       <>
-
+       <Person name={people[0].name} age={people[0].age} />
+          <Person name={people[1].name} age={people[1].age}/>
+          <button onClick={changeName}>changeNameForFirstPerson</button>
       </>
   )
 }
---
+++ components-demo/src/components/Person/Person.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import  './Person.css'
+
+interface Props  extends  React.PropsWithChildren{
+    name: string;
+    age: number;
+}
+
+const Person: React.FC<Props> = ({name,age, children}) => {
+    return (
+        <div>
+            <div className="person">
+                <h1>{name}</h1>
+                <p>Age: {age}</p>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Person;
